e16c557e07fbb7e17b25b78428ce2d0d6d0375f1
HanchiAhmed100 <hanchi.ahmed@yahoo.fr>
2021-01-19 16:48:07 +0100

    fix cors

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8100"
+  	origin: "http://localhost:8100",
+	origin: "http://localhost:8080",
   
 };
 
